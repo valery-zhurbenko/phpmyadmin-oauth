f732ec3071b3dd8822e9f52316da5ec554a97d99
Valery <valery@glidetalk.com>
2015-11-05 17:32:26 +0200

    no pass

--- config.inc.php
+++ config.inc.php
@@ -28,10 +28,10 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'testdb.cubrisbupoor.us-east-1.rds.amazonaws.com';
+$cfg['Servers'][$i]['host'] = 'localhost';
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
 $cfg['Servers'][$i]['compress'] = false;
-$cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
 
 /*
  * phpMyAdmin configuration storage settings.
